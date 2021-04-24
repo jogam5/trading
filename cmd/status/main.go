e684e1166c34216cb5d7fa4445ca3058da648971
jogam5 <jogam5@yahoo.com.mx>
2021-04-23 20:27:18 -0500

    refactored status file

--- cmd/status/main.go
+++ cmd/status/main.go
@@ -5,7 +5,9 @@ This program contains a function that is executed via a cron job. Every specific
 package main
 
 import (
+	"log"
 	"trading/client"
+	"trading/models"
 	"trading/spreadsheet"
 )
 
@@ -13,36 +15,26 @@ func main() {
 	sh := client.ConnectionGoogle("1MK6SUfDrVHQXWL7pUZzS3yxkWuIDecAvHqxXpSKHWL8")
 	bfxPriv, _ := client.ConnectionBitfinex()
 
-	timestamp := "18:00:00"
+	timestamp := "19:00:00"
+	assets := []models.Asset{
+		models.Asset{Name: "ETH"},
+		models.Asset{Name: "BTC"},
+		models.Asset{Name: "LTC"},
+		models.Asset{Name: "LINK"},
+		models.Asset{Name: "ADA"},
+		models.Asset{Name: "ALGO"},
+		models.Asset{Name: "ATOM"},
+		models.Asset{Name: "DOT"},
+		models.Asset{Name: "XRP"},
+	}
 
-	sheetETH, _ := sh.SheetByTitle("ETH")
-	spreadsheet.MonitorOrderStatus(bfxPriv, sheetETH, timestamp)
+	for _, v := range assets {
+		log.Println(v.Name)
+		sheet, _ := sh.SheetByTitle(v.Name)
+		spreadsheet.MonitorOrderStatus(bfxPriv, sheet, timestamp)
+	}
 
 	sheetETHFund, _ := sh.SheetByTitle("ETH-FUND")
 	spreadsheet.MonitorOrderStatus(bfxPriv, sheetETHFund, timestamp)
-
-	sheetBTC, _ := sh.SheetByTitle("BTC")
-	spreadsheet.MonitorOrderStatus(bfxPriv, sheetBTC, timestamp)
-
-	sheetLTC, _ := sh.SheetByTitle("LTC")
-	spreadsheet.MonitorOrderStatus(bfxPriv, sheetLTC, timestamp)
-
-	sheetLINK, _ := sh.SheetByTitle("LINK")
-	spreadsheet.MonitorOrderStatus(bfxPriv, sheetLINK, timestamp)
-
-	sheetADA, _ := sh.SheetByTitle("ADA")
-	spreadsheet.MonitorOrderStatus(bfxPriv, sheetADA, timestamp)
-
-	sheetALGO, _ := sh.SheetByTitle("ALGO")
-	spreadsheet.MonitorOrderStatus(bfxPriv, sheetALGO, timestamp)
-
-	sheetATOM, _ := sh.SheetByTitle("ATOM")
-	spreadsheet.MonitorOrderStatus(bfxPriv, sheetATOM, timestamp)
-
-	sheetDOT, _ := sh.SheetByTitle("DOT")
-	spreadsheet.MonitorOrderStatus(bfxPriv, sheetDOT, timestamp)
-
-	sheetXRP, _ := sh.SheetByTitle("XRP")
-	spreadsheet.MonitorOrderStatus(bfxPriv, sheetXRP, timestamp)
 
 }
